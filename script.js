81245eae8e294388403a24c188b642e6b0b23bae
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-14 20:03:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ class Rectangle {
 		this._width=width;
 		this._height=height;
 	}
-	get getwidth(){
+	get getWidth(){
 		return this._width
 	} 
-	get getheight(){
+	get getHeight(){
 		return this._height
 	}
 	getArea(){
