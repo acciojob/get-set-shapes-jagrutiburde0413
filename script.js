0b273a73a32b2efcb2f5c65ae11d7918daa3cd3a
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-14 20:26:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ class Square extends Rectangle{
   }
 }
 const rect = new Rectangle(5, 10);
-console.log(rect.width;    // output: 5
+console.log(rect.width);    // output: 5
 console.log(rect.height);   // output: 10
 console.log(rect.getArea());     // output: 50
 
 const square = new Square(7);
-console.log(square.width;      // output: 7
-console.log(square.height;     // output: 7
+console.log(square.width);      // output: 7
+console.log(square.height);     // output: 7
 console.log(square.getArea());       // output: 49
 console.log(square.getPerimeter());  // output: 28
 
